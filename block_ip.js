2b53713fa9f5b6a6ab6ec2ab62e853764386fed3
cpm-subsub <cpm_subsub@outlook.jp>
2025-02-11 22:12:48 +0900

    Update block_ip.js

--- block_ip.js
+++ block_ip.js
@@ -1,16 +1,21 @@
 (async function checkIP() {
-    const blockedIPs = ["122.211.63.65"];
+    const blockedIPs = ["233.218.235.61"]; // ブロックしたいIPリスト
 
     try {
-        const response = await fetch('http://ip-api.com/json/');
-        const data = await response.json();
-        const userIP = data.query; // IPアドレス取得
+        const response = await fetch('https://api.ipify.org?format=json');
+        if (!response.ok) throw new Error("IP取得に失敗しました");
 
+        const data = await response.json();
+        const userIP = data.ip;
         console.log("取得したIP:", userIP);
 
         if (blockedIPs.includes(userIP)) {
             alert("アクセスが禁止されています。");
-            window.location.href = "https://www.google.com"; // リダイレクト先
+
+            // 3秒後にリダイレクト
+            setTimeout(() => {
+                window.location.href = "https://www.google.com"; // リダイレクト先
+            }, 3000);
         } else {
             console.log("アクセス許可");
         }
